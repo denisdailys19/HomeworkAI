c3e1c620bdc8796a655ede155eec63a64a3a0567
Ender-slender <enderslenderytx@gmail.com>
2023-10-19 09:40:05 +0800

    LOL

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
+import { Camera, CameraPermissionStatus, useCameraDevice } from 'react-native-vision-camera'
 
 import MainContainer from './src/screens/MainContainer';
 
+const { hasPermission, requestPermission } = useCameraPermission()
+
 export default function App() {
+const device = useCameraDevice('back')
+
+if(device == null) return
   return (
-    <MainContainer/>
+    <Camera />
   );
 }
 
